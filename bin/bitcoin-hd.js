ca394b3e458f9ceb7a5faf03e9bd0fac33fc7752
No Truth <bcnbysytne@gmail.com>
2017-09-09 23:14:03 +0300

    Add skeleton of 'send' command :/

--- bin/bitcoin-hd.js
+++ bin/bitcoin-hd.js
@@ -5,7 +5,7 @@
 // A full-featured framework for building command line applications (cli) with node.js,
 // including help generation, colored output, verbosity control, custom logger, coercion
 // and casting, typos suggestions, and auto-complete for bash/zsh/fish.
-// @see https://github.com/mattallty/Caporal.js
+// @see https://github.com/mattallty/Caporal.js#api
 const prog = require('caporal')
 
 // package.json file
@@ -13,5 +13,19 @@ const pkg = require('../package.json')
 
 prog
   .version(pkg.version)
+  .logger(prog.logger())
+  /**
+   * 'send' command
+   */
+  .command('send', 'Transfer coins (push transaction into current network)')
+  .argument('<address>', 'Destination address')
+  .option('-n, --network <network>', 'Network')
+  .option('-a, --amount <amount>')
+  .action((args, opts, logger) => {
+    logger.info({
+      args,
+      opts
+    })
+  })
 
 prog.parse(process.argv)
